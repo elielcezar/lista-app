4a90a7923cb0d3c94894d1f5232b00df3772f030
Eliel Cezar <elielcezar@gmail.com>
2025-02-13 17:27:26 -0300

    ajustando diretorios para PascalCase pt II

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from '@/context/AuthContext';
 
 // Components
-import Header from '@/components/header';
+import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import ProtectedRoute from '@/components/ProtectedRoute';
 import AddBodyClass from '@/components/AddBodyClass';
